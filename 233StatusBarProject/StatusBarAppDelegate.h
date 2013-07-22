0d2058d5b79e96487e6c0b57a7b10667cdead4e9
fuluchii <chainyzhao@gmail.com>
2013-07-22 22:04:07 +0800

    Initial Commit

---
+++ 233StatusBarProject/StatusBarAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StatusBarAppDelegate.h
+//  233StatusBarProject
+//
+//  Created by fuluchii on 13-7-22.
+//  Copyright (c) 2013年 fuluchii. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface StatusBarAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ 233StatusBarProject/StatusBarAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  StatusBarAppDelegate.m
+//  233StatusBarProject
+//
+//  Created by fuluchii on 13-7-22.
+//  Copyright (c) 2013年 fuluchii. All rights reserved.
+//
+
+#import "StatusBarAppDelegate.h"
+
+@implementation StatusBarAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
